b8db91ac1eba9bfe690468f749dcd74484dff0f0
Maury Miller <maury.miller@ieee.org>
2015-02-21 23:14:19 -0800

    Commit adds help text to application.

--- ui.R
+++ ui.R
@@ -2,12 +2,20 @@ shinyUI(
     pageWithSidebar(
         # Application title
         headerPanel("Compressive Strength prediction using a SVM prediction model"),
+        
         sidebarPanel(
             selectInput("type", label = h4("Select component"), 
                         choices = list("Cement" = 1, "Fine Aggregate" = 2), selected = 1),
             br(),
             numericInput('component', 'kg/m3', 100, min = 100, max = 1000, step = 5),
-            submitButton('Submit')
+            submitButton('Submit'),
+            br(),
+            helpText("Note:This app predicts the compressive strength of concrete with different",
+                     "concentrations of Cement and Fine Aggregate using the svm model.  ",
+                     "The application takes a type of either Cement or Fine Aggregate.  ",  
+                     "It also takes a concentration value in kg/m3. Once a type and concentration", 
+                    "value have been selected click the submit button.  The application will display",
+                    "the input concentration and the predicted compressive strength.")
         ),
         
         mainPanel(
